20cd04549da1548c5d99b5e6cf3ac110dd1bc5df
Jose Vicente Lara <jvlara@opentrends.net>
2018-07-30 14:03:17 +0200

    structure folder and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
+import { LoginComponent } from './login/login.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { APP_ROUTING } from './app.routes';
+import { ReactiveFormsModule } from '@angular/forms';
+import { environment } from '../environments/environment';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    ReactiveFormsModule,
+    APP_ROUTING,
+    BrowserModule, NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
